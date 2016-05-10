0698bb0748c2106237cf82e36054180ae3c7fb8f
dorlandimort <dorlandimort@hotmail.com>
2016-05-09 21:15:26 -0500

    CRUD sitio agregado

--- grails-app/controllers/hackathon/SitioController.groovy
+++ grails-app/controllers/hackathon/SitioController.groovy
@@ -1,9 +1,51 @@
 package hackathon
 
+import grails.converters.JSON
 import org.springframework.security.access.annotation.Secured
 
 @Secured(['ROLE_ADMIN'])
 class SitioController {
 
     def index() { }
+
+    def create() {
+        def sitio = new Sitio(nombre: params.nombre, direccion: params.direccion)
+        if (sitio.save() && ! sitio.hasErrors()) {
+            flash.message = "Sitio creado correctamente"
+            redirect(controller: 'sitio', action: 'sitio', id: sitio.id)
+        }
+    }
+
+    def update(Long id) {
+        def sitio = Sitio.get(id)
+        sitio.nombre = params.nombre
+        sitio.direccion = params.direccion
+        if (sitio.save() && ! sitio.hasErrors()) {
+            flash.message = "Sitio actualizado correctamente"
+            redirect(controller: 'sitio', action: 'sitio', id: sitio.id)
+        }
+
+    }
+
+    def sitio(Long id) {
+        def sitio = Sitio.get(id)
+        render sitio as JSON
+    }
+
+    def sitios() {
+        def sitios = Sitio.getAll()
+        render sitios as JSON
+    }
+
+    def show(Long id) {
+        def sitio = Sitio.get(id)
+        [sitio: sitio]
+    }
+
+    def list() {
+        def sitios = Sitio.getAll()
+        [sitios: sitios]
+    }
+
+
 }
