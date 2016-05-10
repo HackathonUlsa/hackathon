a626ad8f01501cc37726235dc8fffd657c0854bb
GARGUI <garciaguillenalejandro@gmail.com>
2016-05-09 21:26:50 -0500

    ok

--- grails-app/controllers/hackathon/TaxiController.groovy
+++ grails-app/controllers/hackathon/TaxiController.groovy
@@ -1,6 +1,44 @@
 package hackathon
 
+import grails.converters.JSON
+
 class TaxiController {
 
     def index() {}
+
+    def agregarTaxi(){
+        def chofer = Chofer.findById(params.idd)
+        new Taxi(numero: params.numero, numPlaca: params.numPlaca, numPermiso: params.numPermiso, estado: params.estado, capacidadDisponible: params.capacidadDisponible, chofer: chofer).save(flush: true)
+    }
+
+    def editarTaxi(){
+        def chofer = Chofer.findById(params.idd)
+        new Taxi(numero: params.numero, numPlaca: params.numPlaca, numPermiso: params.numPermiso, estado: params.estado, capacidadDisponible: params.capacidadDisponible, chofer: chofer).save(flush: true)
+    }
+
+    def eliminarTaxi(){
+        def taxi = Taxi.findById(params.idd)
+        taxi.delete()
+    }
+
+    def obtenerTaxis(){
+        def taxis = Taxi.getAll()
+        [taxis: taxis]
+    }
+
+    def taxi(Long id){
+        def taxi = Taxi.findById(id)
+        render taxi as JSON
+    }
+
+    def taxis(){
+        def taxis = Taxi.getAll()
+        render taxis as JSON
+    }
+
+    def getBySitio(String sitio){
+        def taxis = Taxi.findBySitio(sitio)
+        render taxis as JSON
+    }
+
 }
